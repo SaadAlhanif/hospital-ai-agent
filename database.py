02c42e06bd9384f10755479ee3c9d5a273d89d4e
SaadAlhanif <Alhanifsaad@gmail.com>
2026-03-08 22:48:53 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,20 @@
 import sqlite3
 
-conn = sqlite3.connect("clinic.db", check_same_thread=False)
-cursor = conn.cursor()
+def init_db():
+    conn = sqlite3.connect("hospital.db", check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS appointments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        date TEXT,
+        time TEXT,
+        reason TEXT,
+        status TEXT DEFAULT 'Confirmed'
+    )
+    """)
+    conn.commit()
+    conn.close()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS appointments (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-name TEXT,
-date TEXT,
-time TEXT,
-reason TEXT
-)
-""")
-
-conn.commit()
+if __name__ == "__main__":
+    init_db()
